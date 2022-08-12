340ea27842eafa5cd4f9313fd25ff5aa4508b331
Sangmin <101931428+netco97@users.noreply.github.com>
2022-08-13 01:39:55 +0900

    mysql 연동

---
+++ login_mysql/src/config/db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host : 'localhost', // 데이터베이스 서버가 있는 주소
+    user : 'root',
+    password : 'dlfdlf07!',
+    database : 'login_lecture' // 사용할 데이터베이스 이름
+});
+
+db.connect();
+
+module.exports =db;
+
