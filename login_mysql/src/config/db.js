27ad12dacd75cbc2595680c490ac2ef43db02129
Sangmin <101931428+netco97@users.noreply.github.com>
2022-08-13 22:46:11 +0900

    Update db.js

--- login_mysql/src/config/db.js
+++ login_mysql/src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host : 'localhost', // 데이터베이스 서버가 있는 주소
     user : 'root',
-    password : 'dlfdlf07!',
+    password : '본인 패스워드',
     database : 'login_lecture' // 사용할 데이터베이스 이름
 });
 
